80ee5eb7426a00c65585154acc98519da288afd4
Ameybairolu <ameybairolu@gmail.com>
2022-10-10 12:30:51 +0530

    Update API key

--- src/js/model.js
+++ src/js/model.js
@@ -1,55 +1,56 @@
-import { AJAX } from './helper.js';
+import { AJAX } from "./helper.js";
 
 //NOTE: All data stored in the state object
 export let state = {
-    search: {
-        query: '',
-        results: []
-    },
-    bookmarks: []
+	search: {
+		query: "",
+		results: [],
+	},
+	bookmarks: [],
 };
 
 // NOTE: Obtain data and store it or return it when the data is searched through an ID
 
 export const loadSearchResults = async function (query, bool) {
-    try {
-        // const link = bool ? ``
-        const data = await AJAX(bool ? `https://superheroapi.com/api.php/4629652520424565/${query}` : `https://superheroapi.com/api.php/4629652520424565/search/${query}`);
+	try {
+		// const link = bool ? ``
+		const data = await AJAX(
+			bool
+				? `https://superheroapi.com/api.php/5668073463249670/${query}`
+				: `https://superheroapi.com/api.php/5668073463249670/search/${query}`,
+		);
 
+		if (bool) return data;
 
-        if (bool) return data;
-
-        state.search.query = query;
-        state.search.results = data.results.map(
-            eachHero => {
-                return {
-                    id: eachHero.id,
-                    name: eachHero.name,
-                    appearance: eachHero.appearance,
-                    biography: eachHero.biography,
-                    image: eachHero.image,
-                    powerstats: eachHero.powerstats,
-                    connections: eachHero.connections,
-                    work: eachHero.work
-                }
-            }
-        );
-    } catch (err) {
-        throw new Error(`Couldn't search for your query. Please try again`);
-    }
-}
+		state.search.query = query;
+		state.search.results = data.results.map((eachHero) => {
+			return {
+				id: eachHero.id,
+				name: eachHero.name,
+				appearance: eachHero.appearance,
+				biography: eachHero.biography,
+				image: eachHero.image,
+				powerstats: eachHero.powerstats,
+				connections: eachHero.connections,
+				work: eachHero.work,
+			};
+		});
+	} catch (err) {
+		throw new Error(`Couldn't search for your query. Please try again`);
+	}
+};
 
 // NOTE: Logic to add data to bookmarks or to remove it
 
 export const addIdToBookmarks = function (id) {
-    if (state.bookmarks.includes(id)) {
-        const index = state.bookmarks.indexOf(id);
-        state.bookmarks.splice(index, 1);
-    } else {
-        state.bookmarks.push(id);
-    }
-}
+	if (state.bookmarks.includes(id)) {
+		const index = state.bookmarks.indexOf(id);
+		state.bookmarks.splice(index, 1);
+	} else {
+		state.bookmarks.push(id);
+	}
+};
 
 export const updateAllElementsBeforeStart = function (data) {
-    state = data;
-}
+	state = data;
+};
